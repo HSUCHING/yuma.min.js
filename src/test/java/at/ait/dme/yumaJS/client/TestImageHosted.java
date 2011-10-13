5c279ee98a2272177be52726450dc7b8cac61797
Rainer Simon <rainer.simon@ait.ac.at>
2011-10-13 11:12:45 +0200

    Image example now uses AIT-hosted demo annotation server

--- src/test/java/at/ait/dme/yumaJS/client/TestImageHosted.java
+++ src/test/java/at/ait/dme/yumaJS/client/TestImageHosted.java
@@ -34,7 +34,7 @@ public class TestImageHosted implements EntryPoint {
 				'edit': 'BEARBEITEN',
 				'delete': 'L\u00d6SCHEN'
 			},
-			serverURL:"http://localhost:8080/yuma4j-server"
+			serverURL:"http://dme.ait.ac.at/yuma4j-server"
 		};
 	}-*/;
 
