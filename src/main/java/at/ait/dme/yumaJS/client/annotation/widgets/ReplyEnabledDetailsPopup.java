b2d731ff2db1bd3b34b5475c8c6c12d93dad93d4
Rainer Simon <rainer.simon@ait.ac.at>
2011-10-18 16:36:36 +0200

    'Add comment' field behavior tweak

--- src/main/java/at/ait/dme/yumaJS/client/annotation/widgets/ReplyEnabledDetailsPopup.java
+++ src/main/java/at/ait/dme/yumaJS/client/annotation/widgets/ReplyEnabledDetailsPopup.java
@@ -15,8 +15,8 @@ import com.google.gwt.dom.client.Document;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.event.dom.client.KeyDownEvent;
+import com.google.gwt.event.dom.client.KeyDownHandler;
 import com.google.gwt.event.dom.client.MouseOutEvent;
 import com.google.gwt.event.dom.client.MouseOutHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -42,8 +42,8 @@ public class ReplyEnabledDetailsPopup extends DetailsPopup {
 		replyField = new TextArea();
 		replyField.setStyleName("annotation-popup-add-comment");
 		replyField.getElement().setAttribute("placeholder", "Add a Comment...");
-		replyField.addKeyUpHandler(new KeyUpHandler() {
-			public void onKeyUp(KeyUpEvent event) {
+		replyField.addKeyDownHandler(new KeyDownHandler() {
+			public void onKeyDown(KeyDownEvent event) {
 				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
 					Annotation a = Annotation.create(
 							annotatable.getObjectURI(),
