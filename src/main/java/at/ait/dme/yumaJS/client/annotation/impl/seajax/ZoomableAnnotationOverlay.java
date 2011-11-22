dc91f2bbfcb7040a8b815cb8cc4352527d9ee2fd
rsimon <rainer@no5.at>
2011-11-22 10:44:55 +0100

    Commented out most of Seajax for now to bring things back into compilable state

--- src/main/java/at/ait/dme/yumaJS/client/annotation/impl/seajax/ZoomableAnnotationOverlay.java
+++ src/main/java/at/ait/dme/yumaJS/client/annotation/impl/seajax/ZoomableAnnotationOverlay.java
@@ -3,13 +3,10 @@ package at.ait.dme.yumaJS.client.annotation.impl.seajax;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.Event;
-import com.google.gwt.user.client.EventListener;
-import com.google.gwt.user.client.ui.RootPanel;
 
 import at.ait.dme.yumaJS.client.annotation.Annotatable;
 import at.ait.dme.yumaJS.client.annotation.Annotation;
 import at.ait.dme.yumaJS.client.annotation.impl.image.widgets.BoundingBoxOverlay;
-import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonAnimationHandler;
 import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonPoint;
 import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonRect;
 import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonViewer;
@@ -27,19 +24,21 @@ public class ZoomableAnnotationOverlay {
 	
 	private SeadragonViewer viewer;
 	
-	private BoundingBoxOverlay bboxOverlay;
+	// private BoundingBoxOverlay bboxOverlay;
 	
 	private Element bboxDiv;
 	
-	private InfoPopup detailsPopup;
+	// private InfoPopup detailsPopup;
 	
 	public ZoomableAnnotationOverlay(Annotation annotation, Annotatable annotatable, SeadragonViewer viewer, Labels labels) {
 		this.viewer = viewer;
 		
 		BoundingBox bbox = annotatable.toBoundingBox(annotation.getFragment());
-		bboxOverlay = new BoundingBoxOverlay(null, bbox);
-		bboxDiv = bboxOverlay.getElement();
+		// bboxOverlay = new BoundingBoxOverlay(null, bbox);
+		// bboxDiv = bboxOverlay.getElement();
 		DOM.sinkEvents(bboxDiv, Event.ONMOUSEOVER | Event.ONMOUSEOUT);
+		
+		/*
 		Event.setEventListener(bboxDiv, new EventListener() {
 			public void onBrowserEvent(Event event) {
 				if (event.getTypeInt() == Event.ONMOUSEOVER) {
@@ -56,6 +55,7 @@ public class ZoomableAnnotationOverlay {
 		
 		detailsPopup = new InfoPopup(annotatable, annotation, labels);
 		detailsPopup.setVisible(false);
+		*/
 		
 		SeadragonPoint anchor =
 			viewer.pointFromPixel(SeadragonPoint.create(bbox.getX(), bbox.getY())); 
@@ -70,6 +70,8 @@ public class ZoomableAnnotationOverlay {
 		// TODO this means we're attaching a listener for EVERY annotation 
 		// whereas we really only need to listen for those with visible 
 		// popup-> make this more efficient!
+		
+		/*
 		viewer.addAnimationtListener(new SeadragonAnimationHandler() {
 			public void onAnimation() {
 				if (detailsPopup.isVisible()) {
@@ -81,16 +83,19 @@ public class ZoomableAnnotationOverlay {
 		});
 		
 		RootPanel.get().add(detailsPopup);
+		*/
 	}
 	
+	/*
 	public InfoPopup getDetailsPopup() {
 		return detailsPopup;
 	}
+	*/
 	
 	public void destroy() {
 		viewer.removeOverlay(bboxDiv);
-		bboxOverlay.removeFromParent();
-		detailsPopup.removeFromParent();
+		// bboxOverlay.removeFromParent();
+		// detailsPopup.removeFromParent();
 	}
 
 }
