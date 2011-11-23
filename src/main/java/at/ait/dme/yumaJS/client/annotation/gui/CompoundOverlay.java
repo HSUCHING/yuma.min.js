a3b5b49bdf2993b45b3ba49d81ec310abffc94c6
rsimon <rainer.simon@ait.ac.at>
2011-11-23 22:55:54 +0100

    Architecture revision: DONE.

--- src/main/java/at/ait/dme/yumaJS/client/annotation/gui/CompoundOverlay.java
+++ src/main/java/at/ait/dme/yumaJS/client/annotation/gui/CompoundOverlay.java
@@ -3,6 +3,14 @@ package at.ait.dme.yumaJS.client.annotation.gui;
 import at.ait.dme.yumaJS.client.annotation.Annotation;
 import at.ait.dme.yumaJS.client.annotation.gui.AnnotationWidget.AnnotationWidgetEditHandler;
 
+/**
+ * {@link CompoundOverlay} is a common interface for GUI overlays 
+ * that embody an annotation, or a thread of annotations. In general,
+ * implementations of {@link CompoundOverlay} consist of at least
+ * one {@link AnnotationWidget} and one {@link FragmentWidget}.
+ * 
+ * @author Rainer Simon <rainer.simon@ait.ac.at>
+ */
 public interface CompoundOverlay extends Comparable<CompoundOverlay> {
 	
 	public void setAnnotationWidgetEditHandler(Annotation a, AnnotationWidgetEditHandler handler);
