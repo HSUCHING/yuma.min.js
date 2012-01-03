d4c15a5c02b36d976523a8953021cb3c69433ef7
rsimon <rainer.simon@ait.ac.at>
2012-01-03 14:05:22 +0000

    Starting to fill the gaps in the Seajax Fragment implementation

--- src/main/java/at/ait/dme/yumaJS/client/annotation/impl/seajax/SeajaxFragmentWidget.java
+++ src/main/java/at/ait/dme/yumaJS/client/annotation/impl/seajax/SeajaxFragmentWidget.java
@@ -1,6 +1,7 @@
 package at.ait.dme.yumaJS.client.annotation.impl.seajax;
 
 import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlowPanel;
 
 import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonPoint;
@@ -8,7 +9,9 @@ import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonRect;
 import at.ait.dme.yumaJS.client.annotation.impl.seajax.api.SeadragonViewer;
 import at.ait.dme.yumaJS.client.annotation.ui.FragmentWidget;
 import at.ait.dme.yumaJS.client.annotation.ui.edit.BoundingBox;
+import at.ait.dme.yumaJS.client.annotation.ui.edit.BoundingBoxSelection;
 import at.ait.dme.yumaJS.client.annotation.ui.edit.Range;
+import at.ait.dme.yumaJS.client.annotation.ui.edit.Selection;
 import at.ait.dme.yumaJS.client.annotation.ui.edit.Selection.SelectionChangeHandler;
 
 public class SeajaxFragmentWidget implements FragmentWidget {
@@ -20,7 +23,13 @@ public class SeajaxFragmentWidget implements FragmentWidget {
 	
 	private Element bboxDiv;
 	
-	public SeajaxFragmentWidget(BoundingBox bbox, SeadragonViewer viewer) {
+	private AbsolutePanel editingLayer;
+	
+	private Selection selection;
+	
+	public SeajaxFragmentWidget(BoundingBox bbox, SeadragonViewer viewer, AbsolutePanel editingLayer) {
+		this.editingLayer = editingLayer;
+		
 		SeadragonPoint anchor =
 			viewer.pointFromPixel(SeadragonPoint.create(bbox.getX(), bbox.getY())); 
 		SeadragonPoint bottomRight = 
@@ -56,27 +65,45 @@ public class SeajaxFragmentWidget implements FragmentWidget {
 	}
 
 	public void startEditing() {
-		// TODO Auto-generated method stub
-		
+		// boxMarker.getDiv().getStyle().setVisibility(Visibility.HIDDEN);
+		BoundingBox bbox = getBoundingBox();
+		selection =  new BoundingBoxSelection(editingLayer, bbox);
+		selection.setSelectionChangeHandler(handler);
 	}
 
 	public void cancelEditing() {
-		// TODO Auto-generated method stub
-		
+		selection.destroy();
+		selection = null;
+		// boxMarker.getDiv().getStyle().setVisibility(Visibility.VISIBLE);
 	}
 
 	public void stopEditing() {
-		// TODO Auto-generated method stub
-		
+		setBoundingBox(selection.getSelectedBounds());
+		selection.destroy();
+		selection = null;
+		// boxMarker.getDiv().getStyle().setVisibility(Visibility.VISIBLE);
 	}
 
 	public void setZIndex(int idx) {
-		// TODO Auto-generated method stub
-		
+		// boxMarker.getDiv().getStyle().setZIndex(idx);
 	}
 
-	public int compareTo(FragmentWidget arg0) {
-		// TODO Auto-generated method stub
+	public int compareTo(FragmentWidget other) {
+		if (!(other instanceof SeajaxFragmentWidget))
+			return 0;
+		
+		/*
+		SeajaxFragmentWidget overlay = (SeajaxFragmentWidget) other;
+		
+		int thisArea = boxMarker.getDiv().getOffsetWidth() * boxMarker.getDiv().getOffsetHeight();
+		int otherArea = overlay.boxMarker.getDiv().getOffsetWidth() * overlay.boxMarker.getDiv().getOffsetHeight();
+		
+		if (thisArea > otherArea)
+			return -1;
+
+		if (thisArea < otherArea)
+			return 1;
+		*/
 		return 0;
 	}
 
